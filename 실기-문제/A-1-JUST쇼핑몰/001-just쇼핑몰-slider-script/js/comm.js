9a4aa01799e5083a90f0df39c7c7918a8d11ba24
choi11223344 <choi890220@gmail.com>
2023-08-04 11:04:00 +0900

    230804-slide play

--- 실기-문제/A-1-JUST쇼핑몰/001-just쇼핑몰-slider-script/js/comm.js
+++ 실기-문제/A-1-JUST쇼핑몰/001-just쇼핑몰-slider-script/js/comm.js
@@ -41,18 +41,46 @@ $(function () {
 
   // slide
   let now = 1;
+  const slideEls = $(".lst-slide");
+  let height = 100;
 
   // 3초마다 실행 - setInterval(함수, 시간);
   setInterval(function () {
     slide();
   }, 3000);
 
+  // 슬라이드 함수(기능 만들기)
   function slide() {
     console.log("dd");
-    // .lst-slide를 에니메이션(top:-300px)
-    // 3초 후 -300px(-100%) 6초 후 -600px(-200%)
-    $(".lst-slide").animate({
-      top: 100 * now * -1 + "%",
-    });
+
+    // 조건문
+    // now: 0일 경우 - 1 슬라이드
+    // now: 1일 경우 - 2 슬라이드
+    // now: 2일 경우 - 3 슬라이드
+    // 참: 만약 1,2번째 슬라이드일 경우
+    // 거짓(그 밖의 경우) : 3번 슬라이드일 경우
+    if (now < 3) {
+      // 참일 경우
+      // 다음 슬라이드로 이동
+      // .lst-slide를 에니메이션(top:-300px)
+      // 3초 후 -300px(-100%) 6초 후 -600px(-200%)
+      slideEls.animate({
+        top: height * now * -1 + "%",
+        // = top: 300 * now * -1 + "px"
+      });
+      // -> -100% 값
+
+      // 변수 재선언
+      now = now + 1;
+      // -> now값 2 / -200%
+    } else {
+      // 거짓일 경우
+      // 첫번째 슬라이드로 이동
+      slideEls.animate({
+        top: 0,
+      });
+      now = 1;
+      // now값 다시 1로 재셋팅
+    }
   }
 });
